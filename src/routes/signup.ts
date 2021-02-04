42a8d60a1af5920baa0858ad7deadf1b3a9b8118
EnderDev <kieran@dothq.co>
2021-02-04 17:20:09 +0000

    📝 Update identity API

--- src/routes/signup.ts
+++ src/routes/signup.ts
@@ -5,7 +5,7 @@ import argon from 'argon2';
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.post("/api/id/accounts/signup", async (req, res) => {
+router.post("/api/identity/accounts/signup", async (req, res) => {
     const { email, password, name } = req.body
 
     await prisma.users.create({
