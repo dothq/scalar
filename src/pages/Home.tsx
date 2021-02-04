e91d7e01d1c3a026251af085ec98a44198cc403e
trickypr <trickypr@users.noreply.github.com>
2021-02-04 18:25:34 +1100

    💬 Add content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import { Layout } from '../components/Layout'
+import { Layout } from "../components/Layout";
 
 class Home extends React.Component {
   render() {
@@ -9,15 +9,63 @@ class Home extends React.Component {
         <div id="home">
           <h1>Take back your privacy.</h1>
           <p>
-          Dot Browser is a privacy-conscious web browser with smarts built-in for protection against trackers and advertisements online.
+            Dot Browser is a privacy-conscious web browser with smarts built-in
+            for protection against trackers and advertisements online.
+          </p>
+
+          <div>{/* Buttons go here */}</div>
+
+          <img src="/assets/landing-showcase.png" alt="Dot Browser design" />
+
+          <h2>
+            We built a privacy browser in a world where your personal data is
+            sold to the highest bidder.
+          </h2>
+          <p>
+            Your data is constantly being sold through large advertisement
+            networks tracking what sites you like to visit online.
           </p>
 
           <div>
-            {/* Buttons go here */}
+            <div>
+              <h3>Respects your privacy</h3>
+              <p>
+                We never send telemetry or crash reports without your consent.
+              </p>
+            </div>
+
+            <div>
+              <h3>Make Dot Browser yours</h3>
+              <p>
+                Browse our library of thousands of themes and extensions to get
+                Dot just how you like it.
+              </p>
+            </div>
+
+            <div>
+              <h3>Block nasty ads and trackers</h3>
+              <p>
+                We will offer to mask your email address when you sign up for
+                sites or services.
+              </p>
+            </div>
+
+            <div>
+              <h3>Migrate from Chrome or Edge</h3>
+              <p>
+                Dot can migrate all your data from your previous browser in just
+                a few clicks.
+              </p>
+            </div>
+
+            <div>
+              <h3>Powered by Open Source</h3>
+              <p>
+                Dot is built on top of open-source software meaning the source
+                code is open to anyone.
+              </p>
+            </div>
           </div>
-
-          <img src="/assets/landing-showcase.png" alt="Dot Browser design"/>
-
         </div>
       </Layout>
     );
