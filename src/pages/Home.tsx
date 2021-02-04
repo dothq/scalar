b66828b7d06f892e19804d661315bdaa7f3cc4a5
trickypr <trickypr@users.noreply.github.com>
2021-02-04 19:05:27 +1100

    💄 The home page now looks passable

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,55 @@
 import React from 'react'
 
 import { Layout } from '../components/Layout'
+import style from '../styles/home.module.css'
+import {
+  Lock,
+  Mail,
+  Migrate,
+  OpenSource,
+  Shield,
+  Sparkles,
+} from '../assets/home'
 
 class Home extends React.Component {
   render() {
     return (
       <Layout>
-        <div id="home">
-          <h1>Take back your privacy.</h1>
-          <p>
-            Dot Browser is a privacy-conscious web browser with smarts built-in
-            for protection against trackers and advertisements online.
-          </p>
-
-          <div>{/* Buttons go here */}</div>
-
-          <img src="/assets/landing-showcase.png" alt="Dot Browser design" />
-
-          <h2>
-            We built a privacy browser in a world where your personal data is
-            sold to the highest bidder.
-          </h2>
-          <p>
-            Your data is constantly being sold through large advertisement
-            networks tracking what sites you like to visit online.
-          </p>
-
-          <div>
+        <div className={style.layout}>
+          <div className={style.grid}>
             <div>
+              <h1>Take back your privacy.</h1>
+              <p>
+                Dot Browser is a privacy-conscious web browser with smarts
+                built-in for protection against trackers and advertisements
+                online.
+              </p>
+
+              <div>{/* Buttons go here */}</div>
+            </div>
+
+            <div>
+              <img
+                src="/assets/landing-showcase.png"
+                alt="Dot Browser design"
+              />
+            </div>
+          </div>
+
+          <div className={style.section}>
+            <h2>
+              We built a privacy browser in a world where your personal data is
+              sold to the highest bidder.
+            </h2>
+            <p>
+              Your data is constantly being sold through large advertisement
+              networks tracking what sites you like to visit online.
+            </p>
+          </div>
+
+          <div className={style.features}>
+            <div>
+              <img src={Lock} alt="" />
               <h3>Respects your privacy</h3>
               <p>
                 We never send telemetry or crash reports without your consent.
@@ -35,6 +57,7 @@ class Home extends React.Component {
             </div>
 
             <div>
+              <img src={Sparkles} alt="" />
               <h3>Make Dot Browser yours</h3>
               <p>
                 Browse our library of thousands of themes and extensions to get
@@ -43,6 +66,16 @@ class Home extends React.Component {
             </div>
 
             <div>
+              <img src={Mail} alt="" />
+              <h3>Protect your mailbox</h3>
+              <p>
+                We will offer to mask your email address when you sign up for
+                sites or services.
+              </p>
+            </div>
+
+            <div>
+              <img src={Shield} alt="" />
               <h3>Block nasty ads and trackers</h3>
               <p>
                 We will offer to mask your email address when you sign up for
@@ -51,6 +84,7 @@ class Home extends React.Component {
             </div>
 
             <div>
+              <img src={Migrate} alt="" />
               <h3>Migrate from Chrome or Edge</h3>
               <p>
                 Dot can migrate all your data from your previous browser in just
@@ -59,6 +93,7 @@ class Home extends React.Component {
             </div>
 
             <div>
+              <img src={OpenSource} alt="" />
               <h3>Powered by Open Source</h3>
               <p>
                 Dot is built on top of open-source software meaning the source
