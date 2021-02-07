c705475b2f54c4b2d2e78639ec99cb7232622a8f
trickypr <trickypr@users.noreply.github.com>
2021-02-07 18:58:02 +1100

    ✨ Login page (non-functional)

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -8,6 +8,7 @@ import BrowserThanks from '../../pages/products/browser/thanks'
 
 // DotID stuff
 import Signup from '../../pages/id/Signup'
+import Login from '../../pages/id/Login'
 
 // 404 pages
 import NotFound from '../../pages/404'
@@ -23,6 +24,7 @@ const App = () => (
     {/* DotID stuff */}
     <Redirect from="/register" to="/id/signup" />
     <Route exact path="/id/signup" component={Signup} />
+    <Route exact path="/id/login" component={Login} />
 
     {/* 404 page */}
     <Route component={NotFound} />
