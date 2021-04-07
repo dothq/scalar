2098ba6eb2e2ca6eb13cfc220684de273da9515a
Trevor Thalacker <hello@trevorthalacker.com>
2021-04-07 14:45:06 -0700

    :sparkles: /blog Redirects to the Dot Blog

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,5 +1,6 @@
 const redirects = [
     { from: '/join', to: 'https://invite.gg/dot' },
+    { from: '/blog', to: 'https://medium.com/dot-blog' },
     { from: '/legal/privacy', to: '/about/privacy' },
     { from: '/legal/terms', to: '/about/terms' },
     { from: '/legal/cookies', to: '/about/cookies' },
