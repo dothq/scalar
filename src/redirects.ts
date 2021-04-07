eea4d279ec1b09d120d1d573fc880a6729d3fb55
Trevor Thalacker <hello@trevorthalacker.com>
2021-04-07 14:51:42 -0700

    remove merge headers

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,18 +1,10 @@
 const redirects = [
-<<<<<<< HEAD
     { from: '/join', to: 'https://invite.gg/dot' },
     { from: '/blog', to: 'https://medium.com/dot-blog' },
     { from: '/legal/privacy', to: '/about/privacy' },
     { from: '/legal/terms', to: '/about/terms' },
     { from: '/legal/cookies', to: '/about/cookies' },
     { from: '/legal/gdpr', to: '/about/gdpr' }
-=======
-  { from: '/join', to: 'https://invite.gg/dot' },
-  { from: '/legal/privacy', to: '/about/privacy' },
-  { from: '/legal/terms', to: '/about/terms' },
-  { from: '/legal/cookies', to: '/about/cookies' },
-  { from: '/legal/gdpr', to: '/about/gdpr' },
->>>>>>> 6da0e5e74b40d39f20fef8dcbfdc4662db87b22f
 ]
 
 export default redirects
