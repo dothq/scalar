669eb41db7588432931b79599785bb01680414c5
dothq-robot <72629236+dothq-robot@users.noreply.github.com>
2021-04-14 21:59:58 +0000

    🎨 Enforce code style

--- src/pages/about/privacy/style.ts
+++ src/pages/about/privacy/style.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const PrivacyStyle = createGlobalStyle`
 .hero-content {
@@ -43,4 +43,4 @@ export const PrivacyStyle = createGlobalStyle`
   margin-right: 24rem;
 }
 
-`;
+`
