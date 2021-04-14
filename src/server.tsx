9bac38cce0bf5e3c5506f66d6faf178434bf280c
dothq-robot <72629236+dothq-robot@users.noreply.github.com>
2021-04-14 22:20:03 +0000

    🎨 Enforce code style

--- src/server.tsx
+++ src/server.tsx
@@ -72,8 +72,8 @@ server.use(async (req: express.Request, res: express.Response, next) => {
   )
   const fullUrl = 'https://' + req.get('host') + req.originalUrl
 
-  if(req.get('host') === "new.dothq.co") {
-    res.redirect(fullUrl.replace(/new.dothq.co/, "www.dothq.co"))
+  if (req.get('host') === 'new.dothq.co') {
+    res.redirect(fullUrl.replace(/new.dothq.co/, 'www.dothq.co'))
   }
 
   const meta = `
