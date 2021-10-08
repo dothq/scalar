a81deef5bf728f16848b98f166b65c0efd22a513
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-10-08 20:26:18 +0100

    📱 Make CTA responsive

--- src/components/CTA/index.tsx
+++ src/components/CTA/index.tsx
@@ -6,9 +6,9 @@ export const CTA = () => {
     const t = useTranslations("");
 
     return (
-        <div className={"w-full flex justify-center"}>
-            <div className={"max-w-7xl w-full flex flex-col md:flex-row justify-start"}>
-                <div className={"flex flex-col gap-3 my-24 flex-1 justify-center items-center text-center md:items-start md:text-left"}>
+        <div className={"w-full flex justify-center md:px-8 sm:px-8 px-4"}>
+            <div className={"max-w-7xl w-full flex flex-col lg2:flex-row lg2:justify-center justify-start"}>
+                <div className={"flex flex-col gap-3 mt-20 lg2:my-24 flex-1 justify-center items-center text-center lg2:items-start lg2:text-left"}>
                     <h2 className={"text-3xl"}>{t("cta-sub")}</h2>
                     <h1 className={"text-5xl font-semibold"}>{t("cta-title")}</h1>
 
@@ -17,9 +17,9 @@ export const CTA = () => {
                     </div>
                 </div>
 
-                <div className={"justify-end relative hidden md:flex"}>
-                    <div className={"flex w-full h-full bg-gradient-to-r from-white to-transparent z-10 absolute"}></div>
-                    <img className={"my-8 rounded-md"} style={{ width: "660px" }} src={"/static/images/mockups/browser.jpg"}></img>
+                <div className={"justify-center lg2:justify-end relative hidden sm:flex"}>
+                    <div className={"hidden w-full h-full bg-gradient-to-r from-white to-transparent z-10 absolute lg2:flex"}></div>
+                    <img className={"mt-10 mb-20 lg2:my-8 rounded-md select-none"} style={{ width: "660px" }} src={"/static/images/mockups/browser.jpg"}></img>
                 </div>
             </div>
         </div>
