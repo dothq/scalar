2bdc357d6250d1e71f2e6d1b620500dcf6d65a6a
EnderDev <42723993+EnderDev@users.noreply.github.com>
2021-10-09 23:32:04 +0100

    🔒️ Only allow secure access to comments

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -2,10 +2,16 @@ import React from "react"
 import { Giscus } from "@giscus/react"
 
 export const Comments = () => {
+    const [visible, setVisible] = React.useState(0);
     const [base, setBase] = React.useState("");
 
     React.useEffect(() => {
         setBase(`${window.location.protocol}//${window.location.host}`)
+
+        const domain: any = window.location.host.match(/[a-zA-Z0-9][a-zA-Z0-9-_]+\.[a-zA-Z]{2,11}?$/);
+
+        if(domain && domain[0] == "dothq.co") setVisible(1);
+        else setVisible(2);
 
         setTimeout(() => {
             const url = new URL(window.location.href);
@@ -18,16 +24,18 @@ export const Comments = () => {
 
     return (
         <div className={"border-t border-gray6 py-8 mt-4 max-w-4xl"}>
-            <Giscus
-                repo="dothq/www-blog-comments"
-                repoId="R_kgDOGMImpQ"
+            {visible == 1 && <Giscus
+                repo="dothq-comments/www"
+                repoId="R_kgDOGMMIZg"
                 category="Comments"
-                categoryId="DIC_kwDOGMImpc4B_XL0"
+                categoryId="DIC_kwDOGMMIZs4B_XUH"
                 mapping="pathname"
                 reactionsEnabled={"0"}
                 emitMetadata={"0"}
                 theme={`${base}/api/giscus/index.css` as any}
-            />
+            />}
+
+            {visible == 2 && <>Comments are disabled in this environment.</>}
         </div>
     )
 }
