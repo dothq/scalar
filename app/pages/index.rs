6f9a1cf7d4478827e8c7a2a2d6fe56ed1ad129a7
EnderDev <kieran@dothq.org>
2022-09-09 23:18:32 +0100

    🌐 Add l10n stub

--- app/pages/index.rs
+++ app/pages/index.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+use std::collections::HashMap;
+
 use askama::Template;
 use axum::response::IntoResponse;
 
@@ -9,10 +11,15 @@ use crate::utils::templates::HtmlTemplate;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct LandingTemplate {}
+struct LandingTemplate {
+    l: HashMap<String, String>,
+}
 
 pub async fn index() -> impl IntoResponse {
-    let template = LandingTemplate {};
+    let mut stub = HashMap::new();
+    stub.insert("hello-world".to_string(), "Hello world!".to_string());
+
+    let template = LandingTemplate { l: stub };
 
     HtmlTemplate(template)
 }
