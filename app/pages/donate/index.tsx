074ba8abaf756d8e7b4bc4a43d2d832fc014e13a
EnderDev <kieran@dothq.org>
2022-11-06 23:45:20 +0000

    ✨ Add currency selection card

--- app/pages/donate/index.tsx
+++ app/pages/donate/index.tsx
@@ -2,11 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { negotiateLanguages } from "@fluent/langneg";
+import { parseAcceptLanguage } from "intl-parse-accept-language";
 import { Giving } from "../../components/icons/Giving";
 import Aside from "../../components/ui/Aside";
 import Button from "../../components/ui/Button";
 import Card from "../../components/ui/Card";
+import Select from "../../components/ui/Select";
 import TextField from "../../components/ui/TextField";
+import { PageProps } from "../../types";
+import { getComponentConfig } from "../../utils/data";
 
 export const meta = {
 	title: "Donate",
@@ -16,205 +21,357 @@ export const meta = {
 	css: ["donate.css"]
 };
 
-const DonationFront = () => {
+const DonationFront = ({ req }: PageProps) => {
+	const data = getComponentConfig<any>("donations");
+
+	const allCurrencyLocales = data.fiat.currencies
+		.map((h: any) => h.local_currency_of)
+		.reduce((a: any, b: any) => a.concat(b));
+
+	let defaultCurrency = "unset";
+
+	if (
+		(req.query as any).currency &&
+		data.fiat.currencies.find(
+			(c: any) => c.id == (req.query as any).currency
+		)
+	) {
+		defaultCurrency = (req.query as any).currency;
+	} else {
+		defaultCurrency = data.fiat.currencies.find((c: any) =>
+			c.local_currency_of.includes(
+				negotiateLanguages(
+					parseAcceptLanguage(
+						req.headers["accept-language"]
+					),
+					allCurrencyLocales,
+					{
+						defaultLocale: "unset",
+						strategy: "matching"
+					}
+				)[0]
+			)
+		).id;
+	}
+
 	return (
-		<section className={"fdn-stack donate-app"}>
-			<div className={"donate-aside-container"}>
-				<Aside
-					className={"donate-aside"}
-					header={
+		<>
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `window.SCALAR_DONATION_CONFIG = JSON.parse(\"${JSON.stringify(
+						data
+					).replace(/"/g, '\\"')}\");`
+				}}
+			></script>
+			<section className={"fdn-stack donate-app"}>
+				<div className={"donate-aside-container"}>
+					<Aside
+						className={"donate-aside"}
+						header={
+							<>
+								<Giving colour={"red"} size={2.625} />
+								<div
+									className={
+										"donate-aside-text-container"
+									}
+								>
+									<h2>Donate</h2>
+									<p>
+										You can support our projects
+										financially through donations.
+										<br />
+										<br />
+										Please refer to our FAQ for
+										any questions.
+									</p>
+								</div>
+							</>
+						}
+					>
 						<>
-							<Giving colour={"red"} size={2.625} />
 							<div
 								className={
 									"donate-aside-text-container"
 								}
 							>
-								<h2>Donate</h2>
+								<h3>What is my donation used for?</h3>
 								<p>
-									You can support our projects
-									financially through donations.
+									Your donation will be used towards
+									funding development on Dot
+									Browser, Dot Translate and Dot
+									One.
 									<br />
 									<br />
-									Please refer to our FAQ for any
-									questions.
+									Additionally, long-term volunteers
+									may be paid for the time spent
+									contributing to Dot HQ projects.
+								</p>
+							</div>
+
+							<div
+								className={
+									"donate-aside-text-container"
+								}
+							>
+								<h3>
+									Will my donation stay anonymous?
+								</h3>
+								<p>
+									We have no way of knowing who made
+									a donation. The only identifying
+									features of donations we receive
+									are either the last four digits of
+									your card or your wallet address
+									if paying via cryptocurrency.
+									<br />
+									<br />
+									Our payment processor is Stripe,
+									Inc.
+									<br />
+									<a
+										target={"_blank"}
+										href={
+											"/about/privacy#data-processors"
+										}
+									>
+										How does this affect my
+										privacy?
+									</a>
+									.
 								</p>
 							</div>
 						</>
-					}
-				>
-					<>
-						<div
-							className={"donate-aside-text-container"}
-						>
-							<h3>What is my donation used for?</h3>
-							<p>
-								Your donation will be used towards
-								funding development on Dot Browser,
-								Dot Translate and Dot One.
-								<br />
-								<br />
-								Additionally, long-term volunteers may
-								be paid for the time spent
-								contributing to Dot HQ projects.
-							</p>
-						</div>
+					</Aside>
+				</div>
 
-						<div
-							className={"donate-aside-text-container"}
-						>
-							<h3>Will my donation stay anonymous?</h3>
-							<p>
-								We have no way of knowing who made a
-								donation. The only identifying
-								features of donations we receive are
-								either the last four digits of your
-								card or your wallet address if paying
-								via cryptocurrency.
-								<br />
-								<br />
-								Our payment processor is Stripe, Inc.
-								<br />
-								<a
-									target={"_blank"}
-									href={
-										"/about/privacy#data-processors"
-									}
-								>
-									How does this affect my privacy?
-								</a>
-								.
-							</p>
-						</div>
-					</>
-				</Aside>
-			</div>
-
-			<div className={"donate-flow-container"}>
-				<Card
-					id={"donation-step-1"}
-					header={
-						<>
-							<Giving colour="red" size={2.625} />
-							<h4>Step 1 - Select an amount</h4>
-						</>
-					}
-					footer={
-						<div className={"fdn-stack h flex-1 x-end"}>
-							<Button
-								href={"#donation-step-2"}
-								type={"primary"}
-								colour={"blue"}
+				<div className={"donate-flow-container"}>
+					<Card
+						id={"step-1"}
+						header={
+							<>
+								<Giving colour="red" size={2.625} />
+								<h4>Step 1 - Select your currency</h4>
+							</>
+						}
+						footer={
+							<div
+								className={
+									"fdn-stack h flex-1 x-end gap-md"
+								}
 							>
-								Next
-							</Button>
-						</div>
-					}
-				>
-					<div
-						className={
-							"donate-card-container fdn-stack v gap-md"
+								<Button
+									href={"#step-2"}
+									type={"primary"}
+									colour={"blue"}
+								>
+									Next
+								</Button>
+							</div>
 						}
 					>
-						<div className={"donate-amounts-grid"}>
-							<Button
-								type={"secondary"}
-								fullwidth
-								colour={"red"}
-							>
-								£5
-							</Button>
+						<div
+							className={
+								"donate-card-container fdn-stack v gap-md"
+							}
+						>
+							<Select
+								id="currency_input"
+								name="currency"
+								autocomplete="off"
+								items={() => {
+									let items: any = [
+										{
+											value: "unset",
+											children:
+												"Choose a currency"
+										}
+									];
 
-							<Button
-								type={"secondary"}
-								fullwidth
-								colour={"red"}
-							>
-								£10
-							</Button>
+									if (
+										data.fiat.enabled &&
+										data.fiat.currencies
+									) {
+										data.fiat.currencies.map(
+											(c: any) => {
+												items.push({
+													value: c.id,
+													children: `${
+														c.name
+													} (${c.id.toUpperCase()})`
+												});
+											}
+										);
+									}
 
-							<Button
-								type={"primary"}
-								fullwidth
-								colour={"red"}
-							>
-								£25
-							</Button>
+									items.push({
+										disabled: true
+									});
 
-							<Button
-								type={"secondary"}
-								fullwidth
-								colour={"red"}
-							>
-								£50
-							</Button>
+									if (
+										data.crypto.enabled &&
+										data.crypto.currencies
+									) {
+										data.crypto.currencies.map(
+											(c: any) => {
+												items.push({
+													value: c.id,
+													children: `${c.name} (${c.symbol})`
+												});
+											}
+										);
+									}
 
-							<Button
-								type={"secondary"}
-								fullwidth
-								colour={"red"}
-							>
-								£100
-							</Button>
+									items = items.map((c: any) => ({
+										...c,
+										selected:
+											defaultCurrency == c.value
+									}));
 
-							<Button
-								type={"secondary"}
-								fullwidth
-								colour={"red"}
-							>
-								£200
-							</Button>
-
-							<TextField
-								outerId={"custom_amount"}
-								id={"custom_amount_input"}
-								type={"tel"}
-								prefix={"£"}
+									return items;
+								}}
 							/>
 						</div>
-					</div>
-				</Card>
+					</Card>
 
-				<Card
-					id={"donation-step-2"}
-					header={
-						<>
-							<Giving colour="red" size={2.625} />
-							<h4>Step 2 - Method of payment</h4>
-						</>
-					}
-					footer={
-						<div className={"fdn-stack h flex-1 x-end"}>
-							<Button
-								href={"#donation-step-2"}
-								type={"primary"}
-								colour={"blue"}
+					<Card
+						id={"step-2"}
+						header={
+							<>
+								<Giving colour="red" size={2.625} />
+								<h4>Step 2 - Select an amount</h4>
+							</>
+						}
+						footer={
+							<div
+								className={
+									"fdn-stack h flex-1 x-end gap-md"
+								}
 							>
-								Next
-							</Button>
-						</div>
-					}
-				>
-					<div
-						className={
-							"donate-card-container fdn-stack h gap-md"
+								<Button
+									href={"#step-3"}
+									type={"primary"}
+									colour={"blue"}
+								>
+									Next
+								</Button>
+							</div>
 						}
 					>
-						<Button type={"primary"} colour={"blue"}>
-							Card
-						</Button>
+						<div
+							className={
+								"donate-card-container fdn-stack v gap-md"
+							}
+						>
+							<div className={"donate-amounts-grid"}>
+								<Button
+									type={"secondary"}
+									fullwidth
+									colour={"red"}
+								>
+									£5
+								</Button>
 
-						<Button type={"secondary"} colour={"blue"}>
-							Bank transfer
-						</Button>
+								<Button
+									type={"secondary"}
+									fullwidth
+									colour={"red"}
+								>
+									£10
+								</Button>
 
-						<Button type={"secondary"} colour={"blue"}>
-							Cryptocurrency
-						</Button>
-					</div>
-				</Card>
-			</div>
-		</section>
+								<Button
+									type={"primary"}
+									fullwidth
+									colour={"red"}
+								>
+									£25
+								</Button>
+
+								<Button
+									type={"secondary"}
+									fullwidth
+									colour={"red"}
+								>
+									£50
+								</Button>
+
+								<Button
+									type={"secondary"}
+									fullwidth
+									colour={"red"}
+								>
+									£100
+								</Button>
+
+								<Button
+									type={"secondary"}
+									fullwidth
+									colour={"red"}
+								>
+									£200
+								</Button>
+
+								<TextField
+									outerId={"custom_amount"}
+									id={"custom_amount_input"}
+									type={"tel"}
+									prefix={"£"}
+								/>
+							</div>
+						</div>
+					</Card>
+
+					<Card
+						id={"step-3"}
+						header={
+							<>
+								<Giving colour="red" size={2.625} />
+								<h4>Step 3 - Payment details</h4>
+							</>
+						}
+						footer={
+							<div
+								className={
+									"fdn-stack h flex-1 x-end gap-md"
+								}
+							>
+								<Button
+									href={"#step-4"}
+									type={"primary"}
+									colour={"blue"}
+								>
+									Next
+								</Button>
+							</div>
+						}
+					>
+						<div
+							className={
+								"donate-card-container fdn-stack h gap-md"
+							}
+						>
+							<Button type={"primary"} colour={"blue"}>
+								Card
+							</Button>
+
+							<Button
+								type={"secondary"}
+								colour={"blue"}
+							>
+								Bank transfer
+							</Button>
+
+							<Button
+								type={"secondary"}
+								colour={"blue"}
+							>
+								Cryptocurrency
+							</Button>
+						</div>
+					</Card>
+				</div>
+			</section>
+		</>
 	);
 };
 
--- public/media/js/donations/index.js
+++ public/media/js/donations/index.js
@@ -3,6 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const $ = (...args) => document.querySelector(args);
+const $$ = (...args) => document.querySelectorAll(args);
+
+const inputState = new Map();
+const paymentState = new Map();
 
 const setError = (el, message) => {
 	removeError(el);
@@ -21,26 +25,77 @@ const removeError = (el) => {
 	fdnField.classList.remove("has-error");
 };
 
-const inputState = new Map();
+const formattedCurrency = (num) => {
+	return num.toLocaleString(document.documentElement.lang, {
+		maximumFractionDigits: 2,
+		minimumFractionDigits: 2
+	});
+};
+
+const updateURL = () => {
+	const url = new URL(window.location.href);
+
+	paymentState.forEach((v, k) => {
+		url.searchParams.set(k, v);
+	});
+
+	window.history.replaceState(
+		{},
+		"",
+		url.href.split(url.origin)[1]
+	);
+};
+
+// Reset inputs
+$("#custom_amount_input").value = "";
+
+$("#currency_input").addEventListener("change", (e) => {
+	paymentState.set("currency", e.target.value);
+	updateURL();
+});
 
 $("#custom_amount_input").addEventListener("blur", (e) => {
+	if (!e.target.value.length) return;
+
 	const value = e.target.value;
-	const parsed = parseInt(value);
+	const parsed = parseFloat(value);
+
+	if (!(+value === +value)) {
+		return setError(
+			e.target,
+			"Invalid donation amount, should be a number."
+		);
+	}
 
 	inputState.set(e.target, parsed);
 
-	e.target.value = parsed.toLocaleString(
-		document.documentElement.lang
+	e.target.value = formattedCurrency(parsed);
+
+	const hardLimit = parseFloat(
+		window.SCALAR_DONATION_CONFIG.donation_hard_limit
 	);
 
-	if (parsed >= 100_000) {
-		setError(
+	if (parsed >= hardLimit) {
+		return setError(
 			e.target,
-			"Sorry, we cannot process any donations over £100,000 at the moment."
+			`Sorry, we cannot process any donations over ${formattedCurrency(
+				hardLimit
+			)} at the moment.`
 		);
 	} else {
 		removeError(e.target);
 	}
+
+	$(".donate-amounts-grid")
+		.querySelectorAll(".fdn-button.red.primary")
+		.forEach((b) => {
+			if (b.classList.contains("primary")) {
+				b.classList.replace("primary", "secondary");
+			}
+		});
+
+	paymentState.set("amount", parsed);
+	updateURL();
 });
 
 $("#custom_amount_input").addEventListener("focus", (e) => {
@@ -48,3 +103,29 @@ $("#custom_amount_input").addEventListener("focus", (e) => {
 		e.target.value = inputState.get(e.target);
 	}
 });
+
+const allButtonsInGrid = Array.from(
+	$(".donate-amounts-grid").querySelectorAll(".fdn-button.red")
+);
+
+allButtonsInGrid.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		allButtonsInGrid.map((btn) =>
+			btn.classList.replace("primary", "secondary")
+		);
+
+		e.target.classList.replace("secondary", "primary");
+
+		paymentState.set(
+			"amount",
+			parseFloat(e.target.getAttribute("data-amount"))
+		);
+	});
+});
+
+$$(".donate-flow-form").forEach((f) =>
+	f.addEventListener("submit", (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+	})
+);
