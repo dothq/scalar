d1448c2292d3d23be48c3c54e596ef2a1b22c964
EnderDev <kieran@dothq.org>
2022-11-06 23:44:31 +0000

    ♻️ Refractor forms and inputs to use same base

---
+++ app/components/ui/Field.tsx
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import clsx from "clsx";
+import { cloneElement } from "preact";
+import { JSXInternal } from "preact/src/jsx";
+
+export type FieldProps = {
+	label?: any;
+	prefix?: any;
+	suffix?: any;
+	outerId?: string;
+	size?: FDNSize;
+	id?: string;
+	error?: string;
+} & JSXInternal.HTMLAttributes<HTMLInputElement>;
+
+const Field = ({
+	label,
+	prefix,
+	suffix,
+	outerId,
+	id,
+	size,
+	error,
+	children,
+	...rest
+}: FieldProps & { children: any }) => {
+	size = size ? size : ("md" as any);
+
+	const className = clsx("fdn-field", {
+		"has-prefix": !!prefix,
+		"has-suffix": !!suffix,
+		"has-error": !!error,
+		[size as string]: true
+	});
+
+	const Input = cloneElement(children, {
+		...rest,
+		className: clsx("fdn-internal-input", rest.className || ""),
+		id
+	});
+
+	return (
+		<div id={outerId} className={className}>
+			{label && <label htmlFor={id}>{label}</label>}
+
+			<div className={"fdn-input"}>
+				{prefix && (
+					<span className={"fdn-input-prefix"}>
+						{prefix}
+					</span>
+				)}
+				{Input}
+				{suffix && (
+					<span className={"fdn-input-suffix"}>
+						{suffix}
+					</span>
+				)}
+			</div>
+
+			<span className={"fdn-input-error"}>{error}</span>
+		</div>
+	);
+};
+
+export default Field;
--- app/components/ui/TextField/index.tsx
+++ app/components/ui/TextField/index.tsx
@@ -2,56 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import clsx from "clsx";
-import { JSXInternal } from "preact/src/jsx";
+import Field, { FieldProps } from "../Field";
 
-const TextField = ({
-	label,
-	prefix,
-	suffix,
-	outerId,
-	id,
-	size,
-	error,
-	...rest
-}: {
-	label?: any;
-	prefix?: any;
-	suffix?: any;
-	outerId?: string;
-	size?: FDNSize;
-	id?: string;
-	error?: string;
-} & JSXInternal.HTMLAttributes<HTMLInputElement>) => {
-	size = size ? size : ("md" as any);
-
-	const className = clsx("fdn-field", {
-		"has-prefix": !!prefix,
-		"has-suffix": !!suffix,
-		"has-error": !!error,
-		[size as string]: true
-	});
-
+const TextField = ({ ...rest }: FieldProps) => {
 	return (
-		<div id={outerId} className={className}>
-			{label && <label htmlFor={id}>{label}</label>}
-
-			<div className={"fdn-input"}>
-				{prefix && (
-					<span className={"fdn-input-prefix"}>
-						{prefix}
-					</span>
-				)}
-				<input {...rest} id={id} />
-				{suffix && (
-					<span className={"fdn-input-suffix"}>
-						{suffix}
-					</span>
-				)}
-			</div>
-
-			<span className={"fdn-input-error"}>{error}</span>
-		</div>
+		<Field {...rest}>
+			<input />
+		</Field>
 	);
 };
 
