352065e416cc9b88dad3bc3eee5b0b5fecb0ba44
EnderDev <kieran@dothq.org>
2022-11-06 23:44:48 +0000

    🐛 Let Button use anchor props

--- app/components/ui/Button/index.tsx
+++ app/components/ui/Button/index.tsx
@@ -4,6 +4,7 @@
 
 import clsx from "clsx";
 import { createElement } from "preact";
+import { JSXInternal } from "preact/src/jsx";
 
 interface ButtonProps {
 	colour?: FDNColour;
@@ -20,21 +21,26 @@ const Button = ({
 	colour,
 	fullwidth,
 	href,
-	type
-}: ButtonProps) => {
+	type,
+	...rest
+}: JSXInternal.HTMLAttributes<HTMLAnchorElement> & ButtonProps) => {
 	type = type ? type : "primary";
-	size = size ? size : "md";
+	size = size ? size : ("md" as any);
 
-	const className = clsx("fdn-button", {
-		[size as string]: true,
-		[colour as string]: true,
-		[type as string]: true,
-		"full-width": fullwidth
-	});
+	const className = clsx(
+		"fdn-button",
+		{
+			[size as string]: true,
+			[colour as string]: true,
+			[type as string]: true,
+			"full-width": fullwidth
+		},
+		(rest as any).className || ""
+	);
 
 	return createElement(
 		href ? "a" : "button",
-		{ href, className },
+		{ ...rest, href, className },
 		children
 	);
 };
