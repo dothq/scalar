d290d62220e3f01131d43fad4cc7a1e837a2be79
EnderDev <kieran@dothq.org>
2022-11-20 19:43:56 +0000

    🎉 Start work on landing page with blocks

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,21 +2,19 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import Button from "../components/ui/Button";
+import BrowserLandingIntroBlock from "../components/marketing/browser/LandingIntroBlock";
 import { l } from "../l10n";
 import { PageProps } from "../types";
 
 export const meta = {
-	title: "Privacy for all",
-	description: "Dot HQ"
+	title: l("page-index-title"),
+	description: l("page-index-description")
 };
 
 const Index = ({ lang }: PageProps) => {
 	return (
 		<>
-			<h1>Hello world {l("bingus")}</h1>
-
-			<Button colour={"black"}>Contribute</Button>
+			<BrowserLandingIntroBlock />
 		</>
 	);
 };
