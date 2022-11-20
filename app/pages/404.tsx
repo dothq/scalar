64a7ebdda6d9e06e2a5cd6abeda82c963fdb098e
EnderDev <kieran@dothq.org>
2022-11-20 19:44:06 +0000

    🚚 Use hero on 404 page

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { Hero, HeroBody } from "../components/ui/Hero";
 import { l } from "../l10n";
 
 export const meta = {
@@ -13,9 +14,13 @@ export const meta = {
 const NotFound = () => {
 	return (
 		<>
-			<h1>{l("error-not-found-title")}</h1>
+			<Hero colour={"white"}>
+				<HeroBody>
+					<h2>{l("error-not-found-title")}</h2>
 
-			<p>{l("error-not-found-subtitle")}</p>
+					<p>{l("error-not-found-subtitle")}</p>
+				</HeroBody>
+			</Hero>
 		</>
 	);
 };
