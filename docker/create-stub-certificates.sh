706d20c547fb5ef0afe23f5e1cf192d2f3d48f02
EnderDev <kieran@dothq.org>
2022-12-31 18:07:25 +0000

    🐛 Reboot certbot before creating cert

--- docker/create-stub-certificates.sh
+++ docker/create-stub-certificates.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+docker restart certbot
 docker exec certbot mkdir -p /etc/letsencrypt/live/dothq.org
 docker exec certbot openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/letsencrypt/live/dothq.org/privkey.pem \
