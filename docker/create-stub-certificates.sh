478a72c21700de1bbf368554b51a8b592ce61df7
EnderDev <kieran@dothq.org>
2022-12-31 18:05:42 +0000

    🔨 Add script to create a self signed cert

---
+++ docker/create-stub-certificates.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+docker exec certbot mkdir -p /etc/letsencrypt/live/dothq.org
+docker exec certbot openssl req -x509 -nodes -days 365 -newkey rsa:2048 \\
+    -keyout /etc/letsencrypt/live/dothq.org/privkey.pem \\
+    -out /etc/letsencrypt/live/dothq.org/fullchain.pem \\
+    -subj /CN=dothq.org
