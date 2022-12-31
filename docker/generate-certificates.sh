4282873f9f2b04e19773e2d97c28c5b855e0ec92
EnderDev <kieran@dothq.org>
2022-12-31 23:34:10 +0000

    🐛 Add www.dothq.org to certificate

--- docker/generate-certificates.sh
+++ docker/generate-certificates.sh
@@ -6,5 +6,6 @@ docker exec certbot certbot certonly --webroot -w /var/www/letsencrypt \
     --agree-tos \
     -n \
     -d dothq.org
+    -d www.dothq.org
 
 echo "Run docker restart nginx certbot to load the certificate."
